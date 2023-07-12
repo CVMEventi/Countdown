9d1cfb487e3b9feb33ed98ea375c0ae1d193b693
Matteo Piccina <matteo.piccina@cvm.it>
2023-07-12 19:23:09 +0200

    Fix fork-ts-checker file

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,5 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-export const plugins = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
+module.exports = [
+  new ForkTsCheckerWebpackPlugin()
 ];
