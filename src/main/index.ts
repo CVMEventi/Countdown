8705fd7b42e0a34580defc47c6242bec7df45bad
Matteo Piccina <matteo.piccina@cvm.it>
2023-07-14 18:43:54 +0200

    Add auto update

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,9 @@
 import "v8-compile-cache";
 import { CountdownApp } from './App';
+import updater from "update-electron-app";
+updater({
+  updateInterval: '8 hours',
+});
 
 const countdownApp = new CountdownApp();
 countdownApp.run()
