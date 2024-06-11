262c72227c8c333e0854ce0edaa172eb8dc62e62
Matteo Piccina <matteo.piccina@cvm.it>
2024-06-11 10:26:35 +0200

    Change text on quit dialog to help Windows version

--- src/main/App.ts
+++ src/main/App.ts
@@ -127,7 +127,7 @@ export class CountdownApp {
               buttons: [
                 "Cancel",
                 "Hide",
-                "Close"
+                "Quit"
               ]
             })
 
