eccd20668c68557ce18656bde9e20c796604c81a
Matteo Piccina <matteo.piccina@cvm.it>
2025-04-01 10:10:35 +0200

    Fix migration to new version

--- src/main/Migrations/MoveSettingsToWindow.ts
+++ src/main/Migrations/MoveSettingsToWindow.ts
@@ -2,6 +2,7 @@
 // @ts-nocheck
 import {BaseMigration} from "./BaseMigration.ts";
 import {CloseAction, DEFAULT_TIMER_NAME} from '../../common/config.ts'
+import {ulid} from 'ulid'
 
 export class MoveSettingsToWindow implements BaseMigration {
   migrate(oldConfig: { [key: string]: unknown }): { [key: string]: unknown } {
@@ -24,35 +25,39 @@ export class MoveSettingsToWindow implements BaseMigration {
         setWindowAlwaysOnTop: settings.setWindowAlwaysOnTop,
         closeAction: settings.closeAction ?? CloseAction.Ask,
         startHidden: settings.startHidden ?? false,
-        timers: [{
-          name: DEFAULT_TIMER_NAME,
-          yellowAtOption: settings.yellowAtOption,
-          yellowAtMinutes: settings.yellowAtMinutes,
-          yellowAtPercent: settings.yellowAtPercent,
-          timerDuration: settings.timerDuration,
-          setTimeLive: settings.setTimeLive,
-          stopTimerAtZero: settings.stopTimerAtZero,
-          windows: [{
-            alwaysOnTop: settings.timerAlwaysOnTop,
-            bounds: oldConfig.window,
-            show: {
-              ...(settings.show),
-              hours: settings.showHours,
-            },
-            messageBoxFixedHeight: settings.messageBoxFixedHeight,
-            contentAtReset: settings.contentAtReset,
-            colors: {
-              background: settings.backgroundColor,
-              resetBackground: settings.resetBackgroundColor,
-              text: settings.textColor,
-              timerFinishedText: settings.timerFinishedTextColor,
-              clock: settings.clockColor,
-              clockText: settings.clockTextColor,
-            },
-            pulseAtZero: settings.pulseAtZero,
-            use12HourClock: settings.use12HourClock,
-          }]
-        }]
+        timers: {
+          [ulid()]: {
+            name: DEFAULT_TIMER_NAME,
+            yellowAtOption: settings.yellowAtOption,
+            yellowAtMinutes: settings.yellowAtMinutes,
+            yellowAtPercent: settings.yellowAtPercent,
+            timerDuration: settings.timerDuration,
+            setTimeLive: settings.setTimeLive,
+            stopTimerAtZero: settings.stopTimerAtZero,
+            windows: {
+              [ulid()]: {
+                alwaysOnTop: settings.timerAlwaysOnTop,
+                bounds: oldConfig.window,
+                show: {
+                  ...(settings.show),
+                  hours: settings.showHours,
+                },
+                messageBoxFixedHeight: settings.messageBoxFixedHeight,
+                contentAtReset: settings.contentAtReset,
+                colors: {
+                  background: settings.backgroundColor,
+                  resetBackground: settings.resetBackgroundColor,
+                  text: settings.textColor,
+                  timerFinishedText: settings.timerFinishedTextColor,
+                  clock: settings.clockColor,
+                  clockText: settings.clockTextColor,
+                },
+                pulseAtZero: settings.pulseAtZero,
+                use12HourClock: settings.use12HourClock,
+              }
+            }
+          }
+        }
       }
     };
   }
